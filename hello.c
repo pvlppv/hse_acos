e4807c3bfeda8e5d9343f39a5a8a43399bb13590
Andrei Tatarnikov <andrewt0301@gmail.com>
2021-03-19 19:06:06 +0300

    hello added
    Signed-off-by: Andrei Tatarnikov <andrewt0301@gmail.com>

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) {
+    int num;
+    sscanf(argv[1], "%d", &num);
+    printf("words: %d, file: %s\n", num, argv[2]);
+    int fd = open(argv[2], O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IRGRP|S_IROTH);
+    for (int i = 0; i < num; ++i) {
+        int word;
+        read(0, &word, sizeof(word));
+        write(fd, &word, sizeof(word));
+    }
+    close(fd);
+    return 0;
+}
